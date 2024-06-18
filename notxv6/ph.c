94846f34fa67fab00bd1de921d102b8a99f16ae6
raitozero <shuli.raito@gmail.com>
2024-06-17 22:17:55 -0400

    Lab Thread Done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,9 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock;            // declare a lock
+
+
 struct entry {
   int key;
   int value;
@@ -44,9 +47,11 @@ void put(int key, int value)
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key)
+    if (e->key == key){
       break;
+    }
   }
+  pthread_mutex_lock(&lock);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,7 +59,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock);
 }
 
 static struct entry*
@@ -104,7 +109,7 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+  pthread_mutex_init(&lock, NULL); // initialize the lock
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
